1547dd46b71fb48acc3a6d285c998584236881a2
Anuj Mishra <anujmishraan2005@gmail.com>
2025-05-25 23:57:12 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -9,7 +9,6 @@ app.use(express.static('public'));
 app.use('/api', routes);
 
 
-
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
